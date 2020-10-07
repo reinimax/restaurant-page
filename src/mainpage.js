8c6038409b445dd28aacf251fd1999f71329872e
reini <reinhold.schachner@hotmail.com>
2020-10-07 12:19:15 +0200

    Mainpage is now created in its own js file

---
+++ src/mainpage.js
@@ -0,0 +1,22 @@
+export const renderMainPage = () => {
+
+    const content = document.querySelector("#content");
+    const headLine = document.createElement("h1");
+    const image = document.createElement("img");
+    const description = document.createElement("p");
+
+    headLine.textContent = "The Pizza Temple - Best pizza in town";
+    image.setAttribute("src", "/img/pizza.jpg");
+    image.setAttribute("alt", "Sliced pizza");
+    description.textContent = `Lorem ipsum dolor sit amet, consectetur adipiscing 
+                elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut 
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit 
+                in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
+                deserunt mollit anim id est laborum.`;
+
+    content.appendChild(headLine);
+    content.appendChild(image);
+    content.appendChild(description);
+};
